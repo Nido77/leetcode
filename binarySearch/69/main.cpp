a6250f00c89b242aa82b11a88eaf622dd4a6a6b1
nido77 <2466992143@qq.com>
2024-03-25 16:39:31 +0800

    modify code in q69

--- binarySearch/69/main.cpp
+++ binarySearch/69/main.cpp
@@ -9,12 +9,10 @@ public:
         int mid;
         while (left < right) {
             mid = left + ((right-left) >> 1);
-            if (mid == x / mid) {
+            if (mid <= x / mid) {
                 left = mid + 1;
-            } else if (mid > x / mid){
+            } else {
                 right = mid;
-            } else if (mid < x / mid) {
-                left = mid + 1;
             }
         }
         return (static_cast<long>(left) * left == static_cast<long>(x)) ? left: left-1;
